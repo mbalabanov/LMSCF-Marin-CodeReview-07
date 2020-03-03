50b63893d2b3cc90377dd79237a3c2ad9cfab153
Marin Balabanov <marincomics@gmail.com>
2020-03-03 02:16:16 +0100

    Added filter at the last minute

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -98,7 +98,8 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -138,7 +139,9 @@ import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { travelitems } from '../travelinfos';
 import { CartService } from '../cart.service';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @Component({
   selector: 'app-list',
@@ -11,6 +12,7 @@ import { CartService } from '../cart.service';
 export class ListComponent implements OnInit {
 
   travelitems = travelitems;
+  searchText;
 
   constructor(private route: ActivatedRoute, private cartService: CartService) { }
 
